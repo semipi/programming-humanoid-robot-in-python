58ce8037ea9c54dcaa8b037d0ec5d2ef66f00492
semipi <e-funktion@gmx.de>
2015-11-23 13:47:37 +0100

    Extract constants

--- kinematics/joint_data_provider.py
+++ kinematics/joint_data_provider.py
@@ -5,33 +5,57 @@ Created on Mon Nov 16 17:39:12 2015
 @author: kai & maik
 """
 
-OFFSET = {  'HeadYaw'       : (   0.00,   0.00, 126.50), 
-            'HeadPitch'     : (   0.00,   0.00,   0.00),
-            'LShoulderPitch': (   0.00,  98.00, 100.00),
-            'LShoulderRoll' : (   0.00,   0.00,   0.00),
-            'LElbowYaw'     : ( 105.00,  15.00,   0.00),
-            'LElbowRoll'    : (   0.00,   0.00,   0.00),
-            'RShoulderPitch': (   0.00, -98.00, 100.00),
-            'RShoulderRoll' : (   0.00,   0.00,   0.00),
-            'RElbowYaw'     : ( 105.00, -15.00,   0.00),
-            'RElbowRoll'    : (   0.00,   0.00,   0.00),
-            'LHipYawPitch'  : (   0.00,  50.00, -85.00),
-            'LHipRoll'      : (   0.00,   0.00,   0.00),
-            'LHipPitch'     : (   0.00,   0.00,   0.00),
-            'LKneePitch'    : (   0.00,   0.00,-100.00),
-            'LAnklePitch'   : (   0.00,   0.00,-102.90),
-            'LAnkleRoll'    : (   0.00,   0.00,   0.00),
-            'RHipYawPitch'  : (   0.00, -50.00, -85.00),
-            'RHipRoll'      : (   0.00,   0.00,   0.00),
-            'RHipPitch'     : (   0.00,   0.00,   0.00),
-            'RKneePitch'    : (   0.00,   0.00,-100.00),
-            'RAnklePitch'   : (   0.00,   0.00,-102.90),
-            'RAnkleRoll'    : (   0.00,   0.00,   0.00)
+HEADYAW         = 'HeadYaw'
+HEADPITCH       = 'HeadPitch'
+LSHOULDERPITCH  = 'LShoulderPitch'
+LSHOULDERROLL   = 'LShoulderRoll'
+LELBOWYAW       = 'LElbowYaw'
+LELBOWROLL      = 'LElbowRoll'
+RSHOULDERPITCH  = 'RShoulderPitch'
+RSHOULDERROLL   = 'RShoulderRoll'
+RELBOWYAW       = 'RElbowYaw'
+RELBOWROLL      = 'RElbowRoll'
+LHIPYAWPITCH    = 'LHipYawPitch'
+LHIPROLL        = 'LHipRoll'
+LHIPPITCH       = 'LHipPitch'
+LKNEEPITCH      = 'LKneePitch'
+LANKLEPITCH     = 'LAnklePitch'
+LANKLEROLL      = 'LAnkleRoll'
+RHIPYAWPITCH    = 'RHipYawPitch'
+RHIPROLL        = 'RHipRoll'
+RHIPPITCH       = 'RHipPitch'
+RKNEEPITCH      = 'RKneePitch'
+RANKLEPITCH     = 'RAnklePitch'
+RANKLEROLL      = 'RAnkleRoll'
+
+OFFSET = {  HEADYAW       : (   0.00,   0.00, 126.50), 
+            HEADPITCH     : (   0.00,   0.00,   0.00),
+            LSHOULDERPITCH: (   0.00,  98.00, 100.00),
+            LSHOULDERROLL : (   0.00,   0.00,   0.00),
+            LELBOWYAW     : ( 105.00,  15.00,   0.00),
+            LELBOWROLL    : (   0.00,   0.00,   0.00),
+            RSHOULDERPITCH: (   0.00, -98.00, 100.00),
+            RSHOULDERROLL: (   0.00,   0.00,   0.00),
+            RELBOWYAW     : ( 105.00, -15.00,   0.00),
+            RELBOWROLL    : (   0.00,   0.00,   0.00),
+            LHIPYAWPITCH  : (   0.00,  50.00, -85.00),
+            LHIPROLL      : (   0.00,   0.00,   0.00),
+            LHIPPITCH     : (   0.00,   0.00,   0.00),
+            LKNEEPITCH    : (   0.00,   0.00,-100.00),
+            LANKLEPITCH   : (   0.00,   0.00,-102.90),
+            LANKLEROLL    : (   0.00,   0.00,   0.00),
+            RHIPYAWPITCH  : (   0.00, -50.00, -85.00),
+            RHIPROLL      : (   0.00,   0.00,   0.00),
+            RHIPPITCH     : (   0.00,   0.00,   0.00),
+            RKNEEPITCH    : (   0.00,   0.00,-100.00),
+            RANKLEPITCH   : (   0.00,   0.00,-102.90),
+            RANKLEROLL    : (   0.00,   0.00,   0.00)
          }
          
-CHAINS = { 'Head' : ['HeadYaw', 'HeadPitch'],
-           'LArm' : ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
-		'LLeg' : ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
-		'RLeg' : ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
-		'RArm' : ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll']
+CHAINS = { 'Head' : [HEADYAW, HEADPITCH],
+           'LArm' : [LSHOULDERPITCH, LSHOULDERROLL, LELBOWYAW, LELBOWROLL],
+		'LLeg' : [LHIPYAWPITCH, LHIPROLL, LHIPPITCH, LKNEEPITCH, LANKLEPITCH, LANKLEROLL],
+		'RLeg' : [RHIPYAWPITCH, RHIPROLL, RHIPPITCH, RKNEEPITCH, RANKLEPITCH, RANKLEROLL],
+		'RArm' : [RSHOULDERPITCH, RSHOULDERROLL, RELBOWYAW, RELBOWROLL],
           }
+ 
